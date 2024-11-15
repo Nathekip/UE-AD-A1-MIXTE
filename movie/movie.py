af23f7eb9ced55c2b701178c00f6116fd403bc7d
E211688W <eloi.garnier@etu.univ-nantes.fr>
2024-11-15 08:24:46 +0100

    Movie.py et resolvers.py

--- movie/movie.py
+++ movie/movie.py
@@ -1,23 +1,49 @@
+from flask import Flask, request, jsonify, make_response
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
-from flask import Flask, request, jsonify
-
 import resolvers as r
 
-PORT = 3001
-HOST = '0.0.0.0'
 app = Flask(__name__)
 
-# todo create elements for Ariadne
+PORT = 3200
+HOST = '0.0.0.0'
 
-# root message
+# Create type definitions
+type_defs = load_schema_from_path('movie.graphql')
+
+# Create types
+query = QueryType()
+mutation = MutationType()
+movie = ObjectType('Movie')
+
+# Bind resolvers to fields
+query.set_field('movies', r.get_movies)
+query.set_field('movie_by_id', r.movie_by_id)
+query.set_field('movie_by_title', r.movie_by_title)
+
+mutation.set_field('add_movie', r.add_movie)
+mutation.set_field('update_movie_rate', r.update_movie_rate)
+mutation.set_field('delete_movie', r.delete_movie)
+
+# Create executable schema
+schema = make_executable_schema(type_defs, query, mutation, movie)
+
+# Root message
 @app.route("/", methods=['GET'])
 def home():
     return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
 
-# graphql entry points
+# GraphQL endpoint
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
-    # todo to complete
+    data = request.get_json()
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=None,
+        debug=app.debug
+    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
 
 if __name__ == "__main__":
     print("Server running in port %s"%(PORT))
---
+++ movie/resolvers.py
@@ -0,0 +1,65 @@
+import json
+
+def load_movies():
+    with open('{}/databases/movies.json'.format("."), "r") as file:
+        return json.load(file)["movies"]
+
+def write_movies(movies):
+    with open('{}/databases/movies.json'.format("."), 'w') as f:
+        json.dump({"movies": movies}, f)
+
+# Query resolvers
+def get_movies(_, info):
+    return load_movies()
+
+def movie_by_id(_, info, _id):
+    movies = load_movies()
+    for movie in movies:
+        if str(movie['id']) == str(_id):
+            return movie
+    return None
+
+def movie_by_title(_, info, _title):
+    movies = load_movies()
+    for movie in movies:
+        if str(movie['title']) == str(_title):
+            return movie
+    return None
+
+# Mutation resolvers
+def add_movie(_, info, _id, _title, _director, _rating):
+    movies = load_movies()
+    
+    # Check if movie ID already exists
+    for movie in movies:
+        if str(movie['id']) == str(_id):
+            return None
+
+    new_movie = {
+        "id": _id,
+        "title": _title,
+        "director": _director,
+        "rating": _rating
+    }
+    
+    movies.append(new_movie)
+    write_movies(movies)
+    return new_movie
+
+def update_movie_rate(_, info, _id, _rate):
+    movies = load_movies()
+    for movie in movies:
+        if str(movie['id']) == str(_id):
+            movie['rating'] = _rate
+            write_movies(movies)
+            return movie
+    return None
+
+def delete_movie(_, info, _id):
+    movies = load_movies()
+    for movie in movies:
+        if str(movie['id']) == str(_id):
+            movies.remove(movie)
+            write_movies(movies)
+            return movie
+    return None
