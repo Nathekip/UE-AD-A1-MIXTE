9d7cb90af4066acab22bddb0908486bfbc57ddff
Nathekip <nathaniel.guitton@etudiant.univ-rennes1.fr>
2024-11-21 19:57:00 +0100

    refacto et commentaire de booking.py

--- booking/booking.py
+++ booking/booking.py
@@ -20,10 +20,6 @@ class BookingServicer(booking_pb2_grpc.BookingServicer):
         with open('{}/booking/data/bookings.json'.format("."), "w") as file:
             json.dump({"bookings":self.db}, file, indent=2)
 
-    def create_error(self, context):
-        context.set_code(grpc.StatusCode.NOT_FOUND)
-        return booking_pb2.BookingResponse(userid="",datemovies=[])
-
     def GetBookings(self):
         for booking in self.db:
             yield booking_pb2.BookingResponse(
@@ -41,15 +37,18 @@ class BookingServicer(booking_pb2_grpc.BookingServicer):
             datemovies=[])
 
     def AddBookings(self, request, context):
+        # récupération des arguments de la requête : utilisateur, jour et film de la réservation
         userid_request = request.userid
         date_request = request.date
         movie_request = request.movies
         try:
+            # appel à l'api showtime pour vérifier que le film passe bien le jour demandé
             with grpc.insecure_channel('localhost:3202') as channel:
                 stub = showtime_pb2_grpc.ShowtimeStub(channel)
                 response = get_showtime_by_date(stub, date_request)
                 if movie_request not in response.movies:
                     return self.create_error(context)
+                # vérification dans la database des réservations que la réservation demandée n'existe pas déjà
                 for booking in self.db:
                     if userid_request == booking["userid"]:
                         for date in booking["dates"]:
@@ -57,12 +56,15 @@ class BookingServicer(booking_pb2_grpc.BookingServicer):
                                 for movie in date["movies"]:
                                     if movie == movie_request:
                                         return self.create_error(context)
+                                # création de la réservation si l'utilisateur et la date sont déjà dans la DB
                                 date["movies"].append(movie_request)
                                 self.write_in_database()
                                 return self.create_response(userid_request)
+                            # création de la réservation si l'utilisateur est déjà dans la DB
                         booking["dates"].append({"date":date_request,"movies":[movie_request]})
                         self.write_in_database()
                         return self.create_response(userid_request)
+                # création de la réservation si aucune donnée n'est déjà dans la DB
                 self.db.append({"userid": userid_request,
                                 "dates": [
                                 {
@@ -73,22 +75,23 @@ class BookingServicer(booking_pb2_grpc.BookingServicer):
                             })
                 self.write_in_database()
                 return self.create_response(userid_request)
+        # traitement des erreurs éventuelles
         except grpc.RpcError as e:
             if (e.code() == grpc.StatusCode.NOT_FOUND):
                 return self.create_error(context)
         
+    # création d'un objet BookingResponse vide si la demande d'ajout de réservation échoue
+    def create_error(self, context):
+        context.set_code(grpc.StatusCode.NOT_FOUND)
+        return booking_pb2.BookingResponse(userid="",datemovies=[])
 
+    # création d'un objet BookingResponse affichant les réservations de l'utilisateur concerné si sa demande d'ajout de réservation est validée
     def create_response(self, userid):
+        datemovies=[]
         for booking in self.db:
             if booking["userid"] == userid:
-                print("test userid == userid")
                 datemovies = [booking_pb2.DateMovies(date=schedule["date"],movies=schedule["movies"]) for schedule in booking["dates"]]
-                print("test datemovies")
         return booking_pb2.BookingResponse(userid=userid,datemovies=datemovies)
-
-
-
-
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -96,7 +99,6 @@ def serve():
     server.add_insecure_port('[::]:3102')
     server.start()
     server.wait_for_termination()
-
 
 def get_showtimes(stub):
     showtimes = stub.GetShowtimes(showtime_pb2.Empty())
@@ -107,16 +109,5 @@ def get_showtime_by_date(stub, date):
     showtime = stub.GetShowmovies(Date)
     return showtime
 
-def run():
-    with grpc.insecure_channel('localhost:3202') as channel:
-        stub = showtime_pb2_grpc.ShowtimeStub(channel)
-        print("--------------GetShowtimes--------------")
-        get_showtimes(stub)
-        print("--------------GetShowtimeByDate--------------")
-        date = showtime_pb2.Date(date="20151130")
-        get_showtime_by_date(stub, date)
-
-
 if __name__ == '__main__':
-    #run()
     serve()
